2950d1fd93042e6213107734d1ab1ba08c26e32c
urmi-21 <mrbains21@gmail.com>
2020-01-19 18:04:09 -0600

    add travis

--- tests/test_build.sh
+++ tests/test_build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 testBuild() {
-	python build.py -p sample_data/train/mrna.fa -n sample_data/train/lncrna.fa -o tests/out/build_out -m sample_model -d /home/usingh/work/urmi/hoap/pldb/mydb -t 5 -v -r
+	python build.py -p sample_data/train/mrna.fa -n sample_data/train/lncrna.fa -o tests/out/build_out -m sample_model -d tests/pldb/mydb -t 5 -v -r
 }
 
 testPred() {
-    assertEquals 1 2
+    python prediction.py -i sample_data/test/test.fa -o tests/out/output_dir -p output_file_name -t 6 -d tests/pldb/mydb -m tests/out/build_out/sample_model -v -r
 }
 
 . shunit2-2.1.6/src/shunit2
