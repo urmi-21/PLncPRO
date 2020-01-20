8d4887796f8c45b1a9ee5849f5060811f826f1a5
urmi-21 <mrbains21@gmail.com>
2020-01-19 18:42:49 -0600

    add setup

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 testBuild() {
-	python build.py -p sample_data/train/mrna.fa -n sample_data/train/lncrna.fa -o tests/out/build_out -m sample_model -d tests/pldb/mydb -t 2 -v -r
+	build.py -p sample_data/train/mrna.fa -n sample_data/train/lncrna.fa -o tests/out/build_out -m sample_model -d tests/pldb/mydb -t 2 -v -r
 	assertEquals $? 0
 }
 
 testPred() {
-    python prediction.py -i sample_data/test/test.fa -o tests/out/output_dir -p output_file_name -t 2 -d tests/pldb/mydb -m tests/out/build_out/sample_model -v -r
+    prediction.py -i sample_data/test/test.fa -o tests/out/output_dir -p output_file_name -t 2 -d tests/pldb/mydb -m tests/out/build_out/sample_model -v -r
     assertEquals $? 0
 }
 
