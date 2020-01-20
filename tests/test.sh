ac7b12a50541a6ee13ce9e7fb899433b5f900c94
urmi-21 <mrbains21@gmail.com>
2020-01-19 18:19:55 -0600

    update tests

--- tests/test.sh
+++ tests/test.sh
@@ -2,12 +2,12 @@
 
 testBuild() {
 	python build.py -p sample_data/train/mrna.fa -n sample_data/train/lncrna.fa -o tests/out/build_out -m sample_model -d tests/pldb/mydb -t 2 -v -r
-	assertEquals 0 0
+	assertEquals $? 0
 }
 
 testPred() {
     python prediction.py -i sample_data/test/test.fa -o tests/out/output_dir -p output_file_name -t 2 -d tests/pldb/mydb -m tests/out/build_out/sample_model -v -r
-    assertEquals 1 2
+    assertEquals $? 0
 }
 
 . shunit2-2.1.6/src/shunit2
